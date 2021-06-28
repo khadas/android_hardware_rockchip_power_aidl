aa1717c43aa4ee00abcdf0afb09ee938bf8b4974
Bian Jin chen <kenjc.bian@rock-chips.com>
2021-06-28 09:15:17 +0000

    Upgrading to power aidl V2.
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    Change-Id: Iaf436adf4c975bd98cc6ee20ad3a3e81573f75be

--- Power.cpp
+++ Power.cpp
@@ -248,6 +248,17 @@ ndk::ScopedAStatus Power::isBoostSupported(Boost type, bool* _aidl_return) {
     return ndk::ScopedAStatus::ok();
 }
 
+ndk::ScopedAStatus Power::createHintSession(int32_t, int32_t, const std::vector<int32_t>&, int64_t,
+                                            std::shared_ptr<IPowerHintSession>* _aidl_return) {
+    *_aidl_return = nullptr;
+    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+}
+
+ndk::ScopedAStatus Power::getHintSessionPreferredRate(int64_t* outNanoseconds) {
+    *outNanoseconds = -1;
+    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+}
+
 void Power::performanceBoost(bool on) {
     if (!_boot_complete) {
         ALOGI("RK performance_boost skiped during boot!");
